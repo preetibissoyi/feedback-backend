4128e53e39149d661f224bc04cfa71345f08caa8
preeti bissoyi <115729390+preetibissoyi@users.noreply.github.com>
2025-06-25 16:50:53 +0530

    Update parentFeedback.js

--- server/routes/parentFeedback.js
+++ server/routes/parentFeedback.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const parentFeedbackController = require('../controllers/parentFeedbackController');
 
 // Submit parent feedback
-router.post('/parentfeedback', parentFeedbackController.submitParentFeedback);
+router.post('/parent', parentFeedbackController.submitParentFeedback);
 // Get parent feedback statistics
 router.get('/', parentFeedbackController.getParentFeedbackStats);
 
